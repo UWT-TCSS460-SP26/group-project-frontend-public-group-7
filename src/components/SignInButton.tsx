c0aed076d7dacb52dd399f01a23dd7c8393e9bbd
kassie <kassie.j.whitney92@Gmail.com>
2026-06-04 11:11:05 -0700

    fixed signin bug, it now redirects user back to their original page

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -2,11 +2,18 @@
 
 import { Button } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
+import { usePathname, useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { useMediaRouteLoading } from "@/components/MediaRouteLoadingProvider";
 
 export default function SignInButton() {
   const { showLoadingOverlay } = useMediaRouteLoading();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const callbackUrl = searchParams.toString()
+    ? `${pathname}?${searchParams.toString()}`
+    : pathname;
 
   return (
     <Button
@@ -14,7 +21,7 @@ export default function SignInButton() {
       startIcon={<LoginIcon />}
       onClick={() => {
         showLoadingOverlay();
-        void signIn("tcss460", { callbackUrl: "/search" });
+        void signIn("tcss460", { callbackUrl });
       }}
     >
       Sign in
